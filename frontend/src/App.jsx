0ac3fdd807985a9a74a897fe55963bd6598764e6
Kapildagar <91322878+Kapildagar@users.noreply.github.com>
2024-02-08 12:05:03 +0530

    updated loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,74 +6,94 @@ import { data } from 'autoprefixer';
 import Videonav from './Component/Videonav/Videonav';
 import { Link } from 'react-router-dom';
 import routes from '../Routes/Routes';
+import { Blocks } from 'react-loader-spinner'
 function App() {
- 
-  const [loading,setloading]=useState(false);
-  const[videoData,setvideoData]=useState([]);
-  const [Data,setData]=useState({
-    name:"",
-    filename:""
+
+  const [loading, setloading] = useState(false);
+  const [videoData, setvideoData] = useState([]);
+  const [loadingData, setloadingData] = useState(false);
+  const [Data, setData] = useState({
+    name: "",
+    filename: ""
   });
   // const [name,setname]=useState('');
 
-  useEffect(()=>{
-         (async()=>{
-          const res=await axios.get(`${routes}/Allvideo`);
-          if(res.data.success){
-            setvideoData(res.data.data);
-          }
-          console.log(res);
-         })()
-  },[Data])
+  useEffect(() => {
+    (async () => {
+      setloadingData(true);
+      const res = await axios.get(`${routes}/Allvideo`);
+      if (res.data.success) {
+        setvideoData(res.data.data);
+        setloadingData(false)
+      }
+      console.log(res);
+    })()
+  }, [Data])
 
   console.log(videoData)
 
- const handleChange=(e)=>{
-    const {name,value,files}=e.target;
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
     console.log(value);
     console.log(files)
-       setData((prev)=>(
-        {...prev,[name]:files?files[0]:value}
-       ))
-       
- }
- const handelClick=async()=>{
-  try{
-  console.log(Data);
-      const fd=new FormData();
+    setData((prev) => (
+      { ...prev, [name]: files ? files[0] : value }
+    ))
+
+  }
+  const handelClick = async () => {
+    try {
+      console.log(Data);
+      const fd = new FormData();
       console
       setloading(true)
-      fd.append("name",Data.name)
-      fd.append("videofile",Data.filename);
-      const res=await axios.post(`${routes}/createvideo`,fd)    ;
-      if(res.data.success){
-          setloading(false);
+      fd.append("name", Data.name)
+      fd.append("videofile", Data.filename);
+      const res = await axios.post(`${routes}/createvideo`, fd);
+      if (res.data.success) {
+        setloading(false);
       }
       setData({
-        name:"",
-         filename:""
+        name: "",
+        filename: ""
       })
-  }
-  catch(err){
-    setloading(false);
-  }
+    }
+    catch (err) {
+      setloading(false);
+    }
 
- }
+  }
 
   return (
     <>
+
+
       <div className='bg-gray-700 p-1 mb-[40px] flex justify-between'>
-      <h1 className='text-[30px] font-bold  text-white mx-[10px]'>Video Library</h1>
-      <Link to="/bookmark" className='text-white flex items-center mx-[10px] border-[2px] border-white p-1 rounded-md transition ease-in-out hover:scale-110'>BookMark</Link>
+        <h1 className='text-[30px] font-bold  text-white mx-[10px]'>Video Library</h1>
+        <Link to="/bookmark" className='text-white flex items-center mx-[10px] border-[2px] border-white p-1 rounded-md transition ease-in-out hover:scale-110'>BookMark</Link>
       </div>
-      
+
       <div className='flex flex-col  border-[2px] border-black gap-2 p-2 sm:w-[400px] md:w-[600px] mx-auto rounded-md mb-[60px]  '>
-      <h1 className='text-[30px] font-bold text-center'>Add Videos</h1>
-      <input  className='border-[2px] border-black rounded-md p-1' type='text' value={data.name} name="name" onChange={handleChange} placeholder='Enter the tittle' />
-      <input  className='border-[2px] border-black rounded-md '  type='file' accept='video/*' name="filename" onChange={handleChange} />
-      <button  className='border-none border-black rounded-md bg-blue-600 text-white font-bold text-[20px]'  type='submit'onClick={handelClick}>{loading?"Uploading...":"Submit"}</button>
+        <h1 className='text-[30px] font-bold text-center'>Add Videos</h1>
+        <input className='border-[2px] border-black rounded-md p-1' type='text' value={data.name} name="name" onChange={handleChange} placeholder='Enter the tittle' />
+        <input className='border-[2px] border-black rounded-md ' type='file' accept='video/*' name="filename" onChange={handleChange} />
+        <button className='border-none border-black rounded-md bg-blue-600 text-white font-bold text-[20px]' type='submit' onClick={handelClick}>{loading ? "Uploading..." : "Submit"}</button>
       </div>
-      <Videonav  dataArray={videoData}/>
+      {loadingData ?
+        <div className='sm:flex w-fit mx-auto items-center'>
+          <Blocks
+            height="120"
+            width="120"
+            color="#4fa94d"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            visible={true}
+          />
+          <h1 className='text-[30px] font-bold text-cyan-500'>Loading...</h1></div>
+        :
+        <Videonav dataArray={videoData} />
+      }
     </>
   )
 }
--- frontend/src/Component/Bookmark/Bookmark.jsx
+++ frontend/src/Component/Bookmark/Bookmark.jsx
@@ -3,30 +3,46 @@ import axios from "axios"
 import VideoPlayer from "./Videop";
 import { Link } from "react-router-dom";
 import routes from "../../../Routes/Routes";
-
+import { Blocks } from 'react-loader-spinner'
 const Bookmark = () => {
 
     const [data, setdata] = useState(null);
+    const [loading, setloading] = useState(true);
 
     useEffect(() => {
         (async () => {
+            setloading(true);
             const res = await axios.get(`${routes}/Allvideo`)
             console.log(res);
             const ndata = res.data.data;
             const bookdata = ndata.filter((it) => it.bookmark)
             setdata(bookdata);
+            setloading(false)
             console.log(bookdata)
         })()
     }, [])
 
     return (
         <>
-           <div className='bg-gray-700 p-1 mb-[40px] flex justify-between'>
-           <Link to='/'> <h1 className='text-[30px] font-bold  text-white mx-[10px]'>Video Library</h1></Link>
-     
-      </div>
-      
+            <div className='bg-gray-700 p-1 mb-[40px] flex justify-between'>
+                <Link to='/'> <h1 className='text-[30px] font-bold  text-white mx-[10px]'>Video Library</h1></Link>
+
+            </div>
+
             <h1 className="text-[40px] font-bold w-fit mx-auto">BookMark</h1>
+            {loading?
+            <div className='sm:flex w-fit mx-auto items-center'>
+                <Blocks
+                    height="120"
+                    width="120"
+                    color="#4fa94d"
+                    ariaLabel="blocks-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="blocks-wrapper"
+                    visible={true}
+                />
+                <h1 className='text-[30px] font-bold text-cyan-500'>Loading...</h1></div>
+                :
             <div className="sm:flex  sm:flex-row sm:gap-4  sm:flex-wrap sm:justify-center rounded-md border-white m-[40px] p-4 border-[2px bg-gray-200">
                 {data?.map((itm, index) => {
                     return (
@@ -35,7 +51,7 @@ const Bookmark = () => {
                         </div>
                     )
                 })}
-            </div>
+            </div>}
         </>
     )
 }
